d206401f83afee96b443f26ab78717c42f079140
Maria Gomes <mariapg.dev@gmail.com>
2023-09-05 09:21:58 -0300

    Muda a forma de importação dos módulos

--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
-import conectaAPI from "./conecta-api.js";
+import { conectaAPI } from "./conecta-api.js";
 import { mostraEndereco } from "./exibe-endereco.js";
+import { mostraCep } from "./exibe-cep.js"
 
-conectaAPI();
-mostraEndereco();
+conectaAPI;
+mostraEndereco;
+mostraCep;
