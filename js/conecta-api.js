cdd34b4a49c9877f65440c1a0c292acc378cbea2
Maria Gomes <mariapg.dev@gmail.com>
2023-09-05 09:26:12 -0300

    Conecta a API para buscar CEP à partir do endereço

--- js/conecta-api.js
+++ js/conecta-api.js
@@ -1,9 +1,22 @@
 import { mostraEndereco } from "./exibe-endereco.js";
+import { mostraCep } from "./exibe-cep.js"
 
-export default async function conectaAPI(cep) {
+async function conectaAPIEndereco(cep) {
   const resposta = await fetch(`http://viacep.com.br/ws/${cep}/json/`);
   const respostaConvertida = await resposta.json();
 
   mostraEndereco.exibeEndereco(respostaConvertida)
 }
 
+async function conectaAPICep(endereco) {
+  const resposta = await fetch(`http://viacep.com.br/ws/${endereco.estado}/${endereco.cidade}/${endereco.logradouro}/json/`);
+  const respostaConvertida = await resposta.json();
+
+  mostraCep.exibeCep(respostaConvertida)
+}
+
+
+export const conectaAPI = {
+  conectaAPIEndereco,
+  conectaAPICep
+}
