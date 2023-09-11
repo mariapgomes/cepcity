1d77afb9ac10fc995fe40073ac235e6dbe6366a9
Maria Gomes <mariapg.dev@gmail.com>
2023-09-11 08:28:35 -0300

    Conecta o módulo de exibição dos formulários

--- js/script.js
+++ js/script.js
@@ -1,7 +1,9 @@
 import { conectaAPI } from "./conecta-api.js";
 import { mostraEndereco } from "./exibe-endereco.js";
-import { mostraCep } from "./exibe-cep.js"
+import { mostraCep } from "./exibe-cep.js";
+import mostraForm from "./exibe-form.js";
 
 conectaAPI;
 mostraEndereco;
 mostraCep;
+mostraForm();
