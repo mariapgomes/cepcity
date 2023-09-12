bcd59388e8458480d0260f74c7f0dfce7f5fc1c1
Maria Gomes <mariapg.dev@gmail.com>
2023-09-12 07:40:28 -0300

    Acrescenta atalho de acesso aos formulários

--- js/exibe-form.js
+++ js/exibe-form.js
@@ -4,20 +4,23 @@ export default function mostraForm() {
         formularios = document.querySelectorAll('[data-busca]'),
         formBuscaCep = document.querySelector('[data-busca="CEP"]'),
         formBuscaEndereco = document.querySelector('[data-busca="Endereco"]'),
-        titulo = document.querySelector('[data-titulo="principal"]')
+        titulo = document.querySelector('[data-titulo="principal"]'),
+        navCep = document.querySelector('[data-exibe="form-cep"]'),
+        navEndereco = document.querySelector('[data-exibe="form-endereco"]');
   
   function exibeForm(event) {
     const card = event.currentTarget.getAttribute('data-card');
     formularios.forEach(card => card.classList.remove('form-ativo'));
-
-    if(card === '1') {
+    if(card === '1' || event.currentTarget === navCep) {
       titulo.innerText = 'Buscar por CEP';
       formBuscaCep.classList.add('form-ativo');
-    } else {
+    } else if(card === '2' || event.currentTarget === navEndereco) {
       titulo.innerText = 'Buscar por Endereços';
       formBuscaEndereco.classList.add('form-ativo');
     }
   }
   
   cards.forEach(card => {card.addEventListener('click', exibeForm)});
+  navCep.addEventListener('click', exibeForm);
+  navEndereco.addEventListener('click', exibeForm);
 }
