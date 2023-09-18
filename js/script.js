7b16ff96a93cac7a908a49c515fc9ce61c7c766f
Maria Gomes <mariapg.dev@gmail.com>
2023-09-18 08:43:48 -0300

    Cria o dark mode

--- js/script.js
+++ js/script.js
@@ -2,10 +2,12 @@ import { conectaAPI } from "./conecta-api.js";
 import { mostraEndereco } from "./exibe-endereco.js";
 import { mostraCep } from "./exibe-cep.js";
 import mostraForm from "./exibe-form.js";
-import inicio from "./volta-ao-inicio.js"
+import inicio from "./volta-ao-inicio.js";
+import lightDark from "./light-dark-mode.js";
 
 conectaAPI;
 mostraEndereco;
 mostraCep;
 mostraForm();
 inicio();
+lightDark()
