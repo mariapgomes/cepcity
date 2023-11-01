2266aa8a271af8fab03a0bd1afeb1b690ff58297
Maria Gomes <mariapg.dev@gmail.com>
2023-11-01 10:44:02 -0300

    Cria a validação do formulário de busca por endereços

--- js/script.js
+++ js/script.js
@@ -5,11 +5,16 @@ import { conectaAPI } from "./conecta-api.js";
 import { mostraEndereco } from "./exibe-endereco.js";
 import { mostraCep } from "./exibe-cep.js";
 import iniciaMenuMobile from "./menu-mobile.js";
+import validaFormularioEndereco from "./valida-formulario-endereco.js"
 
 lightDark();
 iniciaMenuMobile();
 mostraForm();
+validaFormularioEndereco();
 inicio();
 conectaAPI;
 mostraEndereco;
 mostraCep;
+
+// !VALIDAR OS FORMULÁRIOS
+// !INSERIR O CLICK FORA PARA FECHAR O MENU
