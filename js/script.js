1999d97c5833abe5d444d584cea4b7f9baf7fe9e
Maria Gomes <mariapg.dev@gmail.com>
2023-11-06 14:44:47 -0300

    Organiza os arquivos

--- js/script.js
+++ js/script.js
@@ -1,12 +1,12 @@
-import lightDark from "./light-dark-mode.js";
-import iniciaMenuMobile from "./menu-mobile.js";
-import mostraForm from "./exibe-form.js";
-import inicio from "./volta-ao-inicio.js";
-import { conectaAPI } from "./conecta-api.js";
-import { mostraEndereco } from "./exibe-endereco.js";
-import { mostraCep } from "./exibe-cep.js";
-import validaFormularioCep from "./valida-formulario-cep.js";
-import validaFormularioEndereco from "./valida-formulario-endereco.js"
+import lightDark from "./modules/light-dark-mode.js";
+import iniciaMenuMobile from "./modules/menu-mobile.js";
+import mostraForm from "./modules/exibe-form.js";
+import inicio from "./modules/volta-ao-inicio.js";
+import { conectaAPI } from "./modules/conecta-api.js";
+import { mostraEndereco } from "./modules/exibe-endereco.js";
+import { mostraCep } from "./modules/exibe-cep.js";
+import validaFormularioCep from "./modules/valida-formulario-cep.js";
+import validaFormularioEndereco from "./modules/valida-formulario-endereco.js"
 
 lightDark();
 iniciaMenuMobile();
